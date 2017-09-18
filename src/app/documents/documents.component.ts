f158b3f8bc404e620b74841cdd79ab0bf822fefa
lk <lucaskeys@gmail.com>
2017-09-18 14:41:56 -0700

    Add styling to documents component

--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -10,24 +10,24 @@ export class DocumentsComponent implements OnInit {
   pageTitle: string = "Document Dashboard";
 
   documents: Document[] = [
-    { 
+    {
       title: "My first doc",
       description: "This is the first description",
       file_url: "http://google.com", updated_at: "09/15/17",
-      image_url: ""
+      image_url: "https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg"
     },
     {
       title: "My second doc",
       description: "This is the second description",
       file_url: "http://google.com",
       updated_at: "09/15/17",
-      image_url: ""
+      image_url: "https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg"
     },
     {
       title: "My third doc",
       description: "This is the third description",
       file_url: "http://google.com", updated_at: "09/15/17",
-      image_url: ""
+      image_url: "https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg"
     }
   ];
 
